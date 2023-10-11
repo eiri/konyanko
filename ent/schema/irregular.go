47c57c853a046826a044a01feae6affbaf048464
Eric Avdey <eiri@eiri.ca>
2023-10-11 17:26:31 -0300

    refactor: add index on view_url in irregular

--- ent/schema/irregular.go
+++ ent/schema/irregular.go
@@ -3,6 +3,7 @@ package schema
 import (
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
 )
 
 // Irregular holds the schema definition for the Irregular entity.
@@ -23,4 +24,11 @@ func (Irregular) Fields() []ent.Field {
 // Edges of the Irregular.
 func (Irregular) Edges() []ent.Edge {
 	return nil
+}
+
+// Indices of the Irregular.
+func (Irregular) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("view_url"),
+	}
 }
