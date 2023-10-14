d972bb3d4abf4e4c463a3d29ef05028bd96f72ba
Eric Avdey <eiri@eiri.ca>
2023-10-14 20:58:13 -0300

    refactor: deep list output starting from animes, not episodes

--- cmd/list.go
+++ cmd/list.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/eiri/konyanko/ent"
 	"github.com/spf13/cobra"
 )
 
@@ -13,23 +14,22 @@ var listCmd = &cobra.Command{
 	Short: "Print all the available episods in JSON format",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := context.Background()
-		episodes, err := client.Episode.
+		titles, err := client.Anime.
 			Query().
-			WithItem().
-			WithTitle().
-			WithReleaseGroup().
+			WithEpisodes(func(q *ent.EpisodeQuery) {
+				q.WithItem()
+				q.WithReleaseGroup()
+			}).
 			All(ctx)
 		if err != nil {
 			return err
 		}
 
-		for _, e := range episodes {
-			data, err := json.MarshalIndent(e, "", "  ")
-			if err != nil {
-				return err
-			}
-			log.Printf("%s\n", data)
+		data, err := json.MarshalIndent(titles, "", "  ")
+		if err != nil {
+			return err
 		}
+		log.Printf("%s\n", data)
 		return nil
 	},
 }
